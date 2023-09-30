3d292f7824a99148f1b0baa6e3c7a710c5859632
Ocast Holdings <desarrollo+mprot@ocastholdings.com>
2023-09-30 11:59:38 -0500

    fixing cart

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,20 +1,29 @@
 import { findProductById } from "./productData.mjs";
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 let product = {};
 
 export default async function productDetails(productId) {
-  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
-  product = await findProductById(productId);
-  // once we have the product details we can render out the HTML
-  renderProductDetails();
-  // once the HTML is rendered we can add a listener to Add to Cart button
-  document.getElementById("addToCart").addEventListener("click", addToCart);
+  try {
+    // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    product = await findProductById(productId);
+    // once we have the product details we can render out the HTML
+    renderProductDetails();
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    document.getElementById("addToCart").addEventListener("click", addToCart);
+  } catch (error) {
+    console.error('Error fetching product details:', error);
+  }
 }
+
 function addToCart() {
-  setLocalStorage("so-cart", product);
+  let products = getLocalStorage("so-cart") == null ? [] : getLocalStorage("so-cart");
+  products.push(product);
+  setLocalStorage("so-cart", products);
 }
+
 function renderProductDetails() {
+  console.log('updating');
   document.querySelector("#productName").innerText = product.Brand.Name;
   document.querySelector("#productNameWithoutBrand").innerText =
     product.NameWithoutBrand;
