f4d3d5e0759d2387a22d9cbef91cfdb1f900ea9f
Miguel Prot <prot.garcia@gmail.com>
2023-11-11 20:54:11 -0500

    listing by quantity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,16 +1,38 @@
 import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+
+  const groupProducts = (products) => {
+    const totalPorId = products.reduce((carry, product) => {
+      if (!carry[product.Id]) {
+        carry[product.Id] = { ...product, quantity: 0 };
+      }
+    
+      // Incrementar la cantidad
+      carry[product.Id].quantity++;
+    console.log(carry);
+      return carry;
+    }, {});
+    
+    // Convertir el objeto de totales de nuevo a un array
+    const resultado = Object.values(totalPorId);
+
+    return resultado;
+  };
+
+  const cartItems = groupProducts(getLocalStorage("so-cart") || []);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const totalItems = cartItems.reduce((total, item) => {return total + item.quantity}, 0);
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector(".list-total").innerHTML = `Total: ${totalItems}`;
+
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimaryLarge}"
       alt="${item.Name}"
     />
   </a>
@@ -18,7 +40,7 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">qty: ${item.quantity}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
